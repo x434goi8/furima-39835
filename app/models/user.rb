7957f1d5f06bfc483f8af2afe44527c1e3c48b7e
x434goi8 <x434goi8@yahoo.co.jp>
2023-10-03 16:22:46 +0900

    新規登録/本人情報確認
    バリデーションを設定した

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, presence: true, format: { with: /^(?=.*[a-zA-Z])(?=.*[0-9])/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠]+\z/ }
+  validates :date_of_birth, presence: true
 end
