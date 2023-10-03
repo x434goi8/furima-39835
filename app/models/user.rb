2a866464713c12512800a4ab626a7b5a2d81fcb3
x434goi8 <x434goi8@yahoo.co.jp>
2023-10-03 16:17:45 +0900

    バリデーションの設定

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :password, presence: true, format: { with: /^(?=.*[a-zA-Z])(?=.*[0-9])/ }
+end
