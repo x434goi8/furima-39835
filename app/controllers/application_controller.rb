f2f25d5df62c4c3de3923d3404298343ec2e0058
x434goi8 <x434goi8@yahoo.co.jp>
2023-10-04 10:35:21 +0900

    ユーザー管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,13 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :password_confirmation, :last_name, :first_name, :last_name_kana, :first_name_kana, :date_of_birth])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, presence: true, format: { with: /^(?=.*[a-zA-Z])(?=.*[0-9])/ }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠]+\z/ }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}\z/ }
   validates :date_of_birth, presence: true
 end
