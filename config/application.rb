b08460818bf221783105ccd9bbbbcad24caef506
x434goi8 <x434goi8@yahoo.co.jp>
2023-10-05 18:08:06 +0900

    テストコード修正

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require_relative "boot"
 
 require "rails/all"
-require 'faker'
-require 'gimei'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
