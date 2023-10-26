eaf72b7ebc120279abca00cc576a990857534b4e
x434goi8 <x434goi8@yahoo.co.jp>
2023-10-26 16:37:46 +0900

    機能修正
    商品詳細機能の確認
    
     商品詳細表示ページは、ログイン状況や商品の販売状況に関係なく、誰でも見ることができること
    ログアウト状態では、商品詳細ページに遷移しようとすると、ログインページに遷移します。ログアウト状態であっても、商品詳細ページに遷移できるように修正をお願いします。
    商品編集機能の確認
    
     必要な情報を適切に入力して「変更する」ボタンを押すと、商品情報（商品画像・商品名・商品の状態など）を編集できること
    もともと登録されている情報が表示されませず、入力した情報も反映されません。登録されている情報が編集フォームに予め入力されているように修正をお願いします。
    商品購入機能の確認
    
     配送先の情報として、郵便番号・都道府県・市区町村・番地・電話番号が必須であること
    都道府県がなくても購入できてしまいます。都道府県は必須項目となるように修正をお願いします。
    
     ログイン状態の場合でも、売却済みの商品には、「商品の編集」「削除」「購入画面に進む」ボタンが表示されないこと
    売却済みの商品にも「購入画面に進む」ボタンが表示されています。表示されないように修正をお願いします。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :destroy, :show, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :destroy, :edit]
   before_action :set_item, only: [:show, :destroy, :edit]
 
   def index
--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -7,7 +7,7 @@ class PurchaseShipping
     validates :item_id
     validates :user_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city_or_district
     validates :street_address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Include hyphen(-)' }
