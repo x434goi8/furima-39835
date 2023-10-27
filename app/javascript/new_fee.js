32314a5ff56b08760a04461bd7fc655853a17bdb
x434goi8 <x434goi8@yahoo.co.jp>
2023-10-27 11:52:51 +0900

    js修正

--- app/javascript/new_fee.js
+++ app/javascript/new_fee.js
@@ -1,24 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const salePriceInput = document.getElementById('item-price');
-  const addTaxPrice = document.getElementById('add-tax-price');
-  const profit = document.getElementById('profit');
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const addTaxDom = document.getElementById("add-tax-price");
+    const addProfitDom = document.getElementById("profit");
 
-  salePriceInput.addEventListener('input', function() {
-    const inputValue = salePriceInput.value;
-    if (!inputValue) {
-      addTaxPrice.textContent = '0';
-      profit.textContent = '0';
-    } else {
-      const salePrice = parseFloat(inputValue);
-      if (isNaN(salePrice)) {
-        addTaxPrice.textContent = 'NaN';
-        profit.textContent = 'NaN';
-      } else {
-        const tax = salePrice * 0.10;
-        const profitAmount = salePrice - tax;
-        addTaxPrice.textContent = Math.floor(tax).toString();
-        profit.textContent = Math.floor(profitAmount).toString();
-      }
+    // 価格を整数として取得
+    const price = parseInt(priceInput.value, 10);
+
+    if (!isNaN(price)) {
+      const tax = Math.ceil(Math.floor(price * 1.1 * 100) / 100); // 税額（誤差を防ぐ）
+      const profit = price - tax; // 利益
+      addTaxDom.innerHTML = tax;
+      addProfitDom.innerHTML = profit;
     }
   });
 });
